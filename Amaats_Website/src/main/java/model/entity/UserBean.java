1ec28c0880238296d769391d9952bc5b056c6e02
emtech-user <emtech-user@192.168.128.107>
2023-05-26 10:10:57 +0900

    UserBeanです。

--- Amaats_Website/src/main/java/model/entity/UserBean.java
+++ Amaats_Website/src/main/java/model/entity/UserBean.java
@@ -21,46 +21,37 @@ public class UserBean implements Serializable {
 		return user_id;
 	}
 
-
 	public void setUser_id(String user_id) {
 		this.user_id = user_id;
 	}
-
 
 	public String getUser_name() {
 		return user_name;
 	}
 
-
 	public void setUser_name(String user_name) {
 		this.user_name = user_name;
 	}
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 
 	public String getUser_genre() {
 		return user_genre;
 	}
 
-
 	public void setUser_genre(String user_genre) {
 		this.user_genre = user_genre;
 	}
 
-
 	public String getUser_address() {
 		return user_address;
 	}
-
 
 	public void setUser_address(String user_address) {
 		this.user_address = user_address;
